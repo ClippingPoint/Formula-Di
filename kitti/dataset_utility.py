b76c9619e0abef2e8441363044dc9ba1d2e5b98b
Ian Zhang <ianzhang1986@gmail.com>
2017-04-07 08:14:24 -0700

    [Feature] Update matrix reshape in loadcamtocam

--- kitti/dataset_utility.py
+++ kitti/dataset_utility.py
@@ -1,5 +1,4 @@
-## Currently loadcalibrationcamtocam and loadcalibrationrigid are exactly same
-# Expand functionality in the future?
+import numpy as np
 def parse_string_variable(str):
     var_name = str.split(':')[0]
     after_colon_index = len(var_name) + 1
@@ -21,6 +20,8 @@ def read_files_by_lines(filename):
         data = cam_to_cam.readlines()
     return read_lines_to_dict(data)
 
+def replace_var_from_dict_with_shape(var_dict, key, shape):
+    var_dict[key] = np.array(var_dict[key]).reshape(shape)
 
 def loadCalibrationCamToCam(filename, verbose=False):
     assert type(filename) is str
@@ -29,15 +30,40 @@ def loadCalibrationCamToCam(filename, verbose=False):
     for key, value in cam_dict.items():
         if key == 'calib_time':
             cam_dict[key] = value
-            if verbose:
-                print(key, value)
         else:
             array = []
             for i, string in enumerate(value.split(' ')[1:]):
                 array.append(float(string))
             cam_dict[key] = array
-            if verbose:
-                print(key, array)
+
+    for i in range(0, 4):
+        S_rect_0i = 'S_rect_0' + str(i)
+        R_rect_0i = 'R_rect_0' + str(i)
+        P_rect_0i = 'P_rect_0' + str(i)
+        S_0i = 'S_0' + str(i)
+        K_0i = 'K_0' + str(i)
+        D_0i = 'D_0' + str(i)
+        R_0i = 'R_0' + str(i)
+        T_0i = 'T_0' + str(i)
+
+        replace_var_from_dict_with_shape(cam_dict, S_rect_0i, (1, 2))
+        replace_var_from_dict_with_shape(cam_dict, R_rect_0i, (3, 3))
+        replace_var_from_dict_with_shape(cam_dict, P_rect_0i, (3, 4))
+        replace_var_from_dict_with_shape(cam_dict, S_0i, (1, 2))
+        replace_var_from_dict_with_shape(cam_dict, K_0i, (3, 3))
+        replace_var_from_dict_with_shape(cam_dict, D_0i, (1, 5))
+        replace_var_from_dict_with_shape(cam_dict, R_0i, (3, 3))
+        replace_var_from_dict_with_shape(cam_dict, T_0i, (3, 1))
+
+    if verbose:
+          print(S_rect_0i, cam_dict[S_rect_0i])
+          print(R_rect_0i, cam_dict[R_rect_0i])
+          print(P_rect_0i, cam_dict[P_rect_0i])
+          print(S_0i, cam_dict[S_0i])
+          print(K_0i, cam_dict[K_0i])
+          print(D_0i, cam_dict[D_0i])
+          print(R_0i, cam_dict[R_0i])
+          print(T_0i, cam_dict[T_0i])
     return cam_dict
 
 def loadCalibrationRigid(filename, verbose=False):
